810c27b63ab6176ff0bac3ae9e7024b83d1f5d33
Hendrik Walter <hwalter014@gmail.com>
2021-04-13 11:21:55 +0200

    added method for file import as String

--- src/Helper.java
+++ src/Helper.java
@@ -1,3 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Helper {
+    public static String readText(String filename){
+        StringBuilder text = new StringBuilder();
+        try{
+            File myFile = new File("..\\text\\"+ filename + ".txt");
+            Scanner scaner = new Scanner(myFile);
+            while (scaner.hasNextLine()){
+                text.append(scaner.nextLine());
+            }
+        }catch (FileNotFoundException e){
+            System.out.println("Fehler gefunden: ");
+            e.printStackTrace();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return text.toString();
+    }
 
 }
