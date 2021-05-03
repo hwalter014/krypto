dbb3b5dbd47a965047cbf0ff190214a4778c670d
Hendrik Walter <hwalter014@gmail.com>
2021-05-03 14:17:45 +0200

    Testausgabe einiger "gefundener" zeichen

--- src/Prak2.java
+++ src/Prak2.java
@@ -21,29 +21,36 @@ public class Prak2 {
             byte currtenRandom = 0;
             byte currentchiffrat = 0;
             byte charDecrypt = 0;
+            boolean foundChar = false;
 
-            while(currtenRandom != 0){
+            //Random.dat Datei einfach durchlaufen
+            while (currtenRandom != -1) {
 
                 currtenRandom = randomInputStream.readByte();
 
+                //chiffrat Datei durchlaufen
                 while (currentchiffrat != -1) {
 
                     currentchiffrat = chiffratInputStream.readByte();
 
+                    //Zeichen entschluesseln
                     charDecrypt = (byte) (currtenRandom ^ currentchiffrat);
 
                     //zuerst gucken ob es Großbuchstabe ist oder ob es Kleinbuchstabe ist
                     //65 ^= A , 90 ^= Z und 65 ^= a , 90 ^= z
-                    if (charDecrypt >= 65 && charDecrypt <= 90 || charDecrypt >= 97 && charDecrypt <= 122){
+                    if (charDecrypt >= 65 && charDecrypt <= 90 || charDecrypt >= 97 && charDecrypt <= 122) {
+                        foundChar = true;
                         System.out.print((char) charDecrypt + " , ");
-                    }else{
+                    } else {
                         break;
                     }
                 }
+                if (foundChar) {
+                    foundChar = false;
+                    System.out.println("neuer Durchlauf folgt");
+                }
 
             }
-
-
 
         } catch (Exception e) {
             e.printStackTrace();
