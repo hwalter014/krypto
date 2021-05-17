bc32d8a687d58e489f01f81575cf550c43b387ff
Hendrik Walter <hwalter014@gmail.com>
2021-05-17 21:51:00 +0200

    idee für LFSR

--- src/Prak3.java
+++ src/Prak3.java
@@ -4,11 +4,25 @@ public class Prak3 {
 
     public static String checkLFSR(int initialwert){
         int[] lfsr = new int[64];
-        for(int i = lfsr.length-1; i > 0; i--){
-            lfsr[i] = lfsr[i-1];
+
+        //initialwert belegen
+        int counter = 0;
+        while(initialwert > 0){
+            lfsr[counter] = initialwert % 2;
+            initialwert = initialwert >> 1;
+            counter++;
         }
-        lfsr[0] = lfsr[21] ^ lfsr[2] ^ lfsr[0];
-        System.out.println(Arrays.toString(lfsr));
+
+        int[] ausgabeLFSR = new int[64];
+
+        for (int ausgabeIndex = 0; ausgabeIndex < ausgabeLFSR.length; ausgabeIndex++) {
+            for (int i = lfsr.length - 1; i > 0; i--) {
+                lfsr[i] = lfsr[i - 1];
+            }
+            lfsr[0] = lfsr[21] ^ lfsr[2] ^ lfsr[0];
+            ausgabeLFSR[ausgabeIndex] = lfsr[0];
+        }
+        System.out.println(Arrays.toString(ausgabeLFSR));
         return "";
     }
 }
