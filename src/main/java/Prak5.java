96fe601c4d7d5740dbddc1e5772907967f970ae0
Hendrik Walter <hwalter014@gmail.com>
2021-06-15 14:02:10 +0200

    vermutung aufgabe a

--- src/main/java/Prak5.java
+++ src/main/java/Prak5.java
@@ -1,2 +1,64 @@
+import java.math.BigInteger;
+import java.util.Random;
+
 public class Prak5 {
+
+    public static void aufgabe1a(int anzahlWdhl){
+
+        int primLaenge = 1500;
+
+        long[] primZeiten = new long[anzahlWdhl];
+
+        //Schleife fuer Anzahl zu generier
+        int primCounter = 0;
+        while(primCounter < anzahlWdhl){
+
+            //Startzeit
+            long startZeit = System.currentTimeMillis();
+
+            //generierung der Primzahl
+            BigInteger prime1 = BigInteger.probablePrime(primLaenge,new Random());
+
+            //Stopzeit
+            long stopZeit = System.currentTimeMillis();
+
+            //Zeit hinzufuegen
+            primZeiten[primCounter] = (stopZeit-startZeit);
+            primCounter++;
+        }
+
+        //Ausgabe der durchschnittszeit
+        System.out.println("Die durchschnittliche Zeit war: " + getAvg(primZeiten));
+
+        //Ausgabe der maximalen Zeit
+        System.out.println("Die laengste Zeit war: " + getMax(primZeiten) + "Millisekunden");
+
+        //Ausgabe der minimalen Zeit
+        System.out.println("Die kuerzeste Zeit war: " + getMin(primZeiten) + "Millisekunden");
+    }
+
+    private static long getMax(long[] val){
+        long max = val[0];
+        for(int i = 1; i < val.length; i++){
+            max = Math.max(max, val[i]);
+        }
+        return max;
+    }
+
+    private static long getMin(long[] val){
+        long min = val[0];
+        for(int i = 1; i < val.length; i++){
+            min = Math.min(min, val[i]);
+        }
+        return min;
+    }
+
+    private static double getAvg(long[] val){
+        float summe = 0;
+        for(long wert : val){
+            summe += wert;
+        }
+        return summe / val.length;
+    }
+
 }
