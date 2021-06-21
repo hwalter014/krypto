253830da8debd8efcc6cf5f465e7da652e8b73c9
Hendrik Walter <hwalter014@gmail.com>
2021-06-21 14:05:09 +0200

    weitere Ideen zur Aufgabe b

--- src/main/java/Prak5.java
+++ src/main/java/Prak5.java
@@ -3,7 +3,7 @@ import java.util.Random;
 
 public class Prak5 {
 
-    public static void aufgabe1a(int anzahlWdhl){
+    public static void aufgabe1a(int anzahlWdhl) {
 
         int primLaenge = 1500;
 
@@ -11,19 +11,19 @@ public class Prak5 {
 
         //Schleife fuer Anzahl zu generier
         int primCounter = 0;
-        while(primCounter < anzahlWdhl){
+        while (primCounter < anzahlWdhl) {
 
             //Startzeit
             long startZeit = System.currentTimeMillis();
 
             //generierung der Primzahl
-            BigInteger prime1 = BigInteger.probablePrime(primLaenge,new Random());
+            BigInteger prime1 = BigInteger.probablePrime(primLaenge, new Random());
 
             //Stopzeit
             long stopZeit = System.currentTimeMillis();
 
             //Zeit hinzufuegen
-            primZeiten[primCounter] = (stopZeit-startZeit);
+            primZeiten[primCounter] = (stopZeit - startZeit);
             primCounter++;
         }
 
@@ -31,31 +31,31 @@ public class Prak5 {
         System.out.println("Die durchschnittliche Zeit war: " + getAvg(primZeiten));
 
         //Ausgabe der maximalen Zeit
-        System.out.println("Die laengste Zeit war: " + getMax(primZeiten) + "Millisekunden");
+        System.out.println("Die laengste Zeit war: " + getMax(primZeiten) + " Millisekunden");
 
         //Ausgabe der minimalen Zeit
-        System.out.println("Die kuerzeste Zeit war: " + getMin(primZeiten) + "Millisekunden");
+        System.out.println("Die kuerzeste Zeit war: " + getMin(primZeiten) + " Millisekunden");
     }
 
-    private static long getMax(long[] val){
+    private static long getMax(long[] val) {
         long max = val[0];
-        for(int i = 1; i < val.length; i++){
+        for (int i = 1; i < val.length; i++) {
             max = Math.max(max, val[i]);
         }
         return max;
     }
 
-    private static long getMin(long[] val){
+    private static long getMin(long[] val) {
         long min = val[0];
-        for(int i = 1; i < val.length; i++){
+        for (int i = 1; i < val.length; i++) {
             min = Math.min(min, val[i]);
         }
         return min;
     }
 
-    private static double getAvg(long[] val){
+    private static double getAvg(long[] val) {
         float summe = 0;
-        for(long wert : val){
+        for (long wert : val) {
             summe += wert;
         }
         return summe / val.length;
@@ -64,31 +64,37 @@ public class Prak5 {
 
     public static void aufgabe1b(){
 
-        boolean primGueltig = false;
+        boolean primeGueltig = false;
         int bitlaenge = 3000;
 
-        BigInteger obereGrenze = new BigInteger("2");
-        BigInteger p = new BigInteger("0");
-        BigInteger q = new BigInteger("0");
+        BigInteger p = null;
+        BigInteger q = null;
 
-        while (!primGueltig){
+        BigInteger valueTwo = new BigInteger("2");
 
-            p = BigInteger.probablePrime(bitlaenge/2, new Random());
-            q = BigInteger.probablePrime(bitlaenge/2, new Random());
+        //Grenzen berechnen
+        BigInteger uGrenze = valueTwo.pow(bitlaenge/2).divide(valueTwo.sqrt());
+        BigInteger oGrenze = valueTwo.pow(bitlaenge/2);
+        //Problem ist dass Grenzen die gleiche Zahl sind.
 
-            //pruefen ob Primzahlen im Intervall liegen
-            primGueltig = (p.subtract(q).compareTo(obereGrenze.pow(bitlaenge / 2).divide(new BigInteger("2").sqrt())) >= 0)
-                    && (p.subtract(q).compareTo(obereGrenze.pow(bitlaenge / 2)) <= 0);
+        while (!primeGueltig){
+            p = BigInteger.probablePrime(new Random().ints(1000,1500).findFirst().getAsInt(), new Random());
+            q = BigInteger.probablePrime(new Random().ints(1000,1500).findFirst().getAsInt(), new Random());
+
+            if(p.compareTo(uGrenze) > 0 && p.compareTo(oGrenze) < 0){
+                if (q.compareTo(uGrenze) > 0 && q.compareTo(oGrenze) < 0){
+                    primeGueltig = true;
+                }
+            }
         }
-
 
         BigInteger n = p.multiply(q);
 
         BigInteger phi = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
 
-        BigInteger e = new BigInteger("2").pow(16).add(BigInteger.ONE);
+        BigInteger e = valueTwo.pow(16).add(BigInteger.ONE);
 
-        BigInteger d = gcd(e, phi);
+        BigInteger d = e.gcd(phi);
         BigInteger f = e.modInverse(phi);
 
         //Ausgabe der Werte
@@ -101,8 +107,8 @@ public class Prak5 {
     }
 
 
-    // extended Euclidean Algorithm
-    private static BigInteger gcd(BigInteger a, BigInteger b){
+    // erweiterter Euklidischer Algorithmus
+    private static BigInteger gcd(BigInteger a, BigInteger b) {
         if (a.compareTo(new BigInteger("0")) == 0)
             return b;
 
